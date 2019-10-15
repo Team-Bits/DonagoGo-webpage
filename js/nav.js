148e36552599367c0ab93f8ed9febd3f599746c1
Adrian Garcia <adriangarlop@hotmail.com>
2019-10-15 09:36:31 -0500

    Added dialogflow component

--- js/nav.js
+++ js/nav.js
@@ -2,8 +2,8 @@ let content = `<div class="align">
 								<a href="./logIn.html">LogIn/SignUp</a>
 								<a href="./index.html">Home</a>
 								<a href="./discover.html" src="">Descubre</a>
-								<a href="./donate.html" src="">Donar</a>
-								<a href="./sell.html" src="">Vender</a>
+								<a href="./donate.html" src="">Publicar</a>
+								<a href="./help.html" src="">Ayuda</a>
 								<div class="form-group row">
 									<div class="col-xs-2">
 										<input class="form-control" placeholder="Buscar" id="ex1" type="text">
