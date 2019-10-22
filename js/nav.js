26966a8bd4dc7f6c14a154b04bcfe22547f5889f
Adrian Garcia <adriangarlop@hotmail.com>
2019-10-21 19:01:36 -0500

    Checkpoint

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,14 @@
-let content = `<div class="align">
-								<a href="./logIn.html">LogIn/SignUp</a>
-								<a href="./index.html">Home</a>
-								<a href="./discover.html" src="">Descubre</a>
-								<a href="./donate.html" src="">Publicar</a>
-								<a href="./help.html" src="">Ayuda</a>
-								<div class="form-group row">
-									<div class="col-xs-2">
-										<input class="form-control" placeholder="Buscar" id="ex1" type="text">
+let content = `	<div class="align">
+									<a href="./logIn.html">LogIn/SignUp</a>
+									<a href="./index.html">Home</a>
+									<a href="./discover.html" src="">Descubre</a>
+									<a href="./donate.html" src="">Publicar</a>
+									<a href="./help.html" src="">Ayuda</a>
+									<div class="form-group row">
+										<div class="col-xs-2">
+											<input class="form-control" placeholder="Buscar" id="ex1" type="text">
+										</div>
 									</div>
-								</div>
-							</div>`;
+								</div>`;
 
 document.getElementById("navbar").innerHTML = content;
