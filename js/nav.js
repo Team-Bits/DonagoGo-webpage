67e5c8f6865eb8a01ddf79fd98d52754ba1676c4
Adrian Garcia <adriangarlop@hotmail.com>
2019-11-06 13:22:07 -0600

    Added search feature in some views

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,35 @@
-let content = `	<div class="nav-align">
-									<a href="./logIn.html">LogIn/SignUp</a>
-									<a href="./index.html">Home</a>
-									<a href="./discover.html" src="">Descubre</a>
-									<a href="./donate.html" src="">Publicar</a>
-									<a href="./help.html" src="">Ayuda</a>
-									<div class="form-group row">
-										<div class="nav-divide">
-											<input class="form-control nav-input" placeholder="" id="ex1" type="text">
-											<button type="button" class="btn btn-success nav-btn" id="nav-search-btn">Buscar</button>
-										</div>
-									</div>
-								</div>`;
+let content = 
+`	<div class="nav-align">
+	<a href="./logIn.html">LogIn/SignUp</a>
+	<a href="./index.html">Home</a>
+	<a href="./discover.html" src="">Descubre</a>
+	<a href="./donate.html" src="">Publicar</a>
+	<a href="./help.html" src="">Ayuda</a>
+	<div class="form-group row">
+		<div class="nav-divide">
+			<input class="form-control nav-input" placeholder="" id="ex1" type="text">
+			<button type="button" class="btn btn-success nav-btn" id="nav-search-btn">Buscar</button>
+		</div>
+	</div>
+</div>`;
+
 document.getElementById("navbar").innerHTML = content;
 
 $("#nav-search-btn").on("click", function(e) {
 
 	e.preventDefault();
 
-	//asd
+	console.log($("#ex1").val());
 
-})
+	$(".search").empty();
+	$(".search").show();
+	$(".notSearch").hide();
+
+	let searchQuery =
+	`
+	<div class="searchTitle">${$("#ex1").val()}</div>
+	`
+
+	$(".search").append(searchQuery);
+
+});
