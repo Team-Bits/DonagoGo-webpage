c6dd31fe2f979bb0e92b5243c0985226a758f3f5
Adrian Garcia <adriangarlop@hotmail.com>
2019-11-16 19:03:52 -0600

    Added upload photo structure. It does not work yet...

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/donagogoDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://donagogoDB";
 exports.PORT = process.env.PORT || 8080;
