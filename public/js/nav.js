377d1a65bf7063e67a7c0e46ac5c13b6ed9a482e
Adrian Garcia <adriangarlop@hotmail.com>
2019-11-21 15:29:33 -0600

    Added toggle

--- public/js/nav.js
+++ public/js/nav.js
@@ -63,6 +63,13 @@ function SearchSearch(id) {
 					
 			$(".search").append(`
 				<div class="divition-all">
+
+					<button data-toggle="collapse" data-target="#demo">Collapsible</button>
+					<div id="demo" class="collapse">
+					Lorem ipsum dolor text....
+					</div>
+
+
 					<div class="divitionSearch">
 						<div class="leftSearch">
 							<img class="searchImage" src="./img/${selectedProduct.image}">
