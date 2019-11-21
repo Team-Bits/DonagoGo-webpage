1e509dbff72a970daf46603d81432c97804801bc
Adrian Garcia <adriangarlop@hotmail.com>
2019-11-21 13:30:29 -0600

    Added cancel policy and fixed bugs at css

--- public/js/orders.js
+++ public/js/orders.js
@@ -27,8 +27,9 @@ function getProducts(id) {
 							</div>
 							<div class="orders-info">
 								<h3 class="order-info-title">${responseJSON[i].name}</h3>
+								<div class="order-info-Description order-info-sub">📍⠀${responseJSON[i].description}</div>
 								<div class="order-info-location order-info-sub">📍⠀${responseJSON[i].location}</div>
-								<div class="order-info order-info-sub">📅 Recoger el ${responseJSON[i].timeSchedule}</div>
+								<div class="order-info order-info-sub">📅 Recoger el Martes a las 8:00 pm</div>
 								<div class="order-info-buttons order-info-sub">
 									<button type="button" class="btn btn-success">Detalles</button>
 									<button type="button" class="btn btn-primary">Calificar</button>
@@ -52,10 +53,28 @@ function getProducts(id) {
 	});
 } getProducts($(".nav-userId").attr('id'));
 
+function getProductForCancel(id) {
+
+	$.ajax({
+
+		url: "/products",
+		method: "GET",
+		dataType: "json",
+
+		success: function(responseJSON) {
+			console.log(responseJSON);
+		},
+
+		error: function(error) {
+			console.log("Juguito de chale", error);
+		}
+	});
+}
+
 function cancelPolicy() {
 	
 	$(".cancel-btn").on("click", function(e) {
 		e.preventDefault();
-		console.log(e.target.id);
+		getProductForCancel(e.target.id);
 	});
 }
